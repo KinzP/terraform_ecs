43e5c55717a81e0400911319549d240cc6d9d8bb
Kinsey Parham <kinseyparham@gmail.com>
2022-08-10 22:49:11 +0000

    changes to cluster

--- main.tf
+++ main.tf
@@ -6,15 +6,18 @@ terraform {
       version = "~> 4.0"
     }
   }
-resource "aws_ecs_cluster_capacity_providers" "cluster" {
-  cluster_name = aws_ecs_cluster.cluster.name
-
+resource "aws_ecs_cluster" "cluster" {
+  name = "KP19-ecs-cluster"
+  
   setting {
     name  = "containerInsights"
     value = "disabled"
   }
 }
-  capacity_providers = ["FARGATE_SPOT", "FARGATE"]
+  resource "aws_ecs_cluster_capacity_providers" "cluster" {
+    cluster_name = aws_ecs_cluster.cluster.name
+  
+    capacity_providers = ["FARGATE_SPOT", "FARGATE"]
 
   default_capacity_provider_strategy {
     capacity_provider = "FARGATE_SPOT"
